79824afa214860b9a7bae7aa8b7a883c8a255c1f
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2022-08-28 00:34:53 +0530

    updated reverse number, all test cases passed. from leetcode

--- Problems/reverse_number.js
+++ Problems/reverse_number.js
@@ -1,13 +1,33 @@
 
 
 const reverse_num = (x) => {
+  var y = x
   x = x.toString()
   var r = []
-  for(let i=0; i<x.length; i++) {
-    r[i] = x[x.length - 1 - i]
+  if(x[0] != '-') {
+    for(let i=0; i<x.length; i++) {
+      r[i] = x[x.length - 1 - i]
+    }
+    var ret = Number(r.join(''))
+    if(ret < 2147483647) return ret
+    else return 0 
+  }else {
+    console.log(x.length)
+    for(let i=0; i<x.length; i++) {
+      if(x[i] == '-') {
+        r[i] = '-'
+      }else {
+        r[i] = x[x.length - i]
+      }
+    } 
+    var ret = Number(r.join(''))
+    if(ret > -2147483648) return ret
+    else return 0 
   }
-  return Number(r.join(''))
 }
 
-result = reverse_num(19991225)
+let num1 = -2147483648
+let num2 = -2147483412
+let result = reverse_num(num)
+// reverse_num(num)
 console.log(result);
